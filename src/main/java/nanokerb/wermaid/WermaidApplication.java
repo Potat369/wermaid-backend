1532f308e2abccb01a128486f963bb7582c77a57
Nanokerb <1102david2007@gmail.com>
2025-01-22 09:52:02 +0200

    Upload project files

---
+++ src/main/java/nanokerb/wermaid/WermaidApplication.java
@@ -0,0 +1,13 @@
+package nanokerb.wermaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WermaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WermaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nanokerb/wermaid/WermaidApplicationTests.java
@@ -0,0 +1,13 @@
+package nanokerb.wermaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WermaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
