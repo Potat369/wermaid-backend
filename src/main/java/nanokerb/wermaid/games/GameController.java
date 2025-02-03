08dfb8c03bff89af3df715c2d8f79b01105827d8
Potat359 <yevheniidemian@gmail.com>
2025-02-03 09:20:04 +0200

    Create get mapping by id for games collection

--- src/main/java/nanokerb/wermaid/games/GameController.java
+++ src/main/java/nanokerb/wermaid/games/GameController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/games")
@@ -19,5 +21,10 @@ public class GameController {
     @GetMapping
     public ResponseEntity<List<Game>> getGames() {
         return new ResponseEntity<>(gameService.getGames(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Game>> getGame(@PathVariable String id) {
+        return new ResponseEntity<>(gameService.getGame(id), HttpStatus.OK);
     }
 }
--- src/main/java/nanokerb/wermaid/games/GameService.java
+++ src/main/java/nanokerb/wermaid/games/GameService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -14,4 +15,7 @@ public class GameService {
         return gameRepository.findAll();
     }
 
+    public Optional<Game> getGame(String id) {
+        return gameRepository.findById(id);
+    }
 }
