e8849b18386a015ebbb4186a02b21224182c1aee
Potat369 <yevheniidemian@gmail.com>
2025-02-24 15:21:38 +0200

    Change fields in Game entity

--- src/main/java/nanokerb/wermaid/games/Game.java
+++ src/main/java/nanokerb/wermaid/games/Game.java
@@ -1,24 +1,20 @@
 package nanokerb.wermaid.games;
 
+import nanokerb.wermaid.ratings.Rating;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Document(collection = "games")
-
 public class Game {
-
-
-
     @Id
     public String id;
     public String name;
     public String description;
-    public Date release_date;
-    public int rating;
-    public int rating_count;
     public String genre;
+    public Date release_date;
+    public String[] links;
     public String picture_url;
-
+    public Rating[] ratings;
 }
