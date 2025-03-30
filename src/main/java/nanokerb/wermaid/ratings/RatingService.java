291cd193b7a8eeb779b37c1f50b29456cfcf4e0f
Nanokerb <1102david2007@gmail.com>
2025-03-30 16:40:43 +0300

    Remove unused methods

--- src/main/java/nanokerb/wermaid/ratings/RatingService.java
+++ src/main/java/nanokerb/wermaid/ratings/RatingService.java
@@ -27,19 +27,6 @@ public class RatingService {
         return repository.findById(id);
     }
 
-    public List<Rating> getByGameId(String gameId) {
-        return repository.findAllByGameId(gameId);
-    }
-
-    public List<Rating> getByUserId(String userId) {
-        return repository.findAllByUserId(userId);
-    }
-
-    public double getAverageRating(String gameId) {
-        List<Rating> ratings = repository.findAllByGameId(gameId);
-        return ratings.stream().mapToInt(r -> r.rating).average().orElse(0.0);
-    }
-
     public void insert(String gameId, RatingRequest rating, String userId) {
         repository.insert(new Rating(gameId, rating.rating, rating.comment, userId));
     }
