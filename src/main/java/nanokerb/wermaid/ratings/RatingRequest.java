7b39e0bb6723115cee47a8b7d27f5a7d26c0bb3a
Nanokerb <1102david2007@gmail.com>
2025-04-01 09:45:38 +0300

    Add validation before saving rating

--- src/main/java/nanokerb/wermaid/ratings/RatingRequest.java
+++ src/main/java/nanokerb/wermaid/ratings/RatingRequest.java
@@ -1,6 +1,13 @@
 package nanokerb.wermaid.ratings;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+
 public class RatingRequest {
+    @Min(0)
+    @Max(5)
     public int rating;
+    @Size(max = 1000)
     public String comment;
 }
